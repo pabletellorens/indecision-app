4eb8116a7f1267ac2c057dc4f0e4738e41b7c0aa
Pablo Llorens <pllomuo@teleco.upv.es>
2025-02-20 20:38:27 +0100

    Instalacion de Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>Mi primera app de Vue</h1>
 
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
   <MeusNumerator :valorem="5" />
 
   <MeusNumeratorScript :valorem="8" />
+
+  <MeusNumeratorScript2 :valorem="9" />
 </template>
 
 <script lang="ts" setup>
+import MeusNumeratorScript2 from './components/meus-numerator-script/MeusNumeratorScript2.vue';
 import MeusNumerator from './components/MeusNumerator.vue';
 import MeusNumeratorScript from './components/MeusNumeratorScript.vue';
 </script>
