989ed4513efea540dafd5b347657d1f792de2602
Pablo Llorens <pllomuo@teleco.upv.es>
2025-02-27 19:36:24 +0100

    v-for de la lista de mensajes

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
-      <ChatBullula :meus-est="true" nuntius="¿Salimos?" />
+      <!--<ChatBullula
+        v-for="nuntius in nuntii"
+        :key="nuntius.id"
+        :meus-est="nuntius.meusEst"
+        :nuntius="nuntius.nuntius"
+        :imago="nuntius.imago"
+      />-->
 
-      <ChatBullula :meus-est="false" 
-      nuntius="no" 
-      imago="https://yesno.wtf/assets/no/24-159febcfd655625c38c147b65e5be565.gif"/>
+      <ChatBullula v-for="nuntius in nuntii" :key="nuntius.id" v-bind="nuntius" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { ChatNuntius } from '@/interfaces/chat-nuntius.interface';
 import ChatBullula from './ChatBullula.vue';
+
+interface Props {
+  nuntii: ChatNuntius[];
+}
+
+defineProps<Props>();
 </script>
--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
     <div class="bg-blue-500 p-4 text-white flex justify-between items-center">Mi esposa</div>
 
-    <ChatNuntii />
+    <ChatNuntii :nuntii="nuntii" />
     <TextusArca />
   </div>
 </template>
@@ -10,4 +10,20 @@
 <script lang="ts" setup>
 import ChatNuntii from '@/components/chat/ChatNuntii.vue';
 import TextusArca from '@/components/chat/TextusArca.vue';
+import type { ChatNuntius } from '@/interfaces/chat-nuntius.interface';
+import { ref } from 'vue';
+
+const nuntii = ref<ChatNuntius[]>([
+  {
+    id: new Date().getTime(),
+    nuntius: '¿Un cafe?',
+    meusEst: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    nuntius: 'No',
+    meusEst: false,
+    imago: 'https://yesno.wtf/assets/no/24-159febcfd655625c38c147b65e5be565.gif',
+  },
+]);
 </script>
