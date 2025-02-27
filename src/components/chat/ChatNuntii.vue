dde3baf6b9ec67ccc39f59d76967aba9c3c4fa40
Pablo Llorens <pllomuo@teleco.upv.es>
2025-02-27 19:04:48 +0100

    Chat con gif de YesNo

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -2,6 +2,10 @@
   <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
       <ChatBullula :meus-est="true" nuntius="¿Salimos?" />
+
+      <ChatBullula :meus-est="false" 
+      nuntius="no" 
+      imago="https://yesno.wtf/assets/no/24-159febcfd655625c38c147b65e5be565.gif"/>
     </div>
   </div>
 </template>
