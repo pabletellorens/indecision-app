18c37e92e3764e884220b8b7cf41e61799fb853a
Pablo Llorens <pllomuo@teleco.upv.es>
2025-03-05 17:05:04 +0100

    Scroll funcionando

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="p-4 flex-1 overflow-y-auto">
+  <div ref="chatRef" class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
-      <!--<ChatBullula
-        v-for="nuntius in nuntii"
-        :key="nuntius.id"
-        :meus-est="nuntius.meusEst"
-        :nuntius="nuntius.nuntius"
-        :imago="nuntius.imago"
-      />-->
-
       <ChatBullula v-for="nuntius in nuntii" :key="nuntius.id" v-bind="nuntius" />
     </div>
   </div>
@@ -17,10 +9,22 @@
 <script lang="ts" setup>
 import type { ChatNuntius } from '@/interfaces/chat-nuntius.interface';
 import ChatBullula from './ChatBullula.vue';
+import { ref, watch } from 'vue';
 
 interface Props {
   nuntii: ChatNuntius[];
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+
+watch(props.nuntii, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
